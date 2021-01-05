bf7c4ea56bfc52dc2b555ae3b1e30d9ba61eba9d
GingerBeardmanJeff <62894495+GingerBeardmanJeff@users.noreply.github.com>
2021-01-05 15:51:35 -0700

    Cloning python-test-model as base

---
+++ g.py
@@ -0,0 +1,37 @@
+#fastscore.schema.0: input_schema.avsc
+#fastscore.schema.1: output_schema.avsc
+
+import json
+import math
+
+# This is a test comment
+
+
+# This is a second test comment
+
+#modelop.init
+def begin():
+    global coefs
+    coefs = json.load(open('external_file_asset.json', 'r'))
+    pass
+
+#modelop.score
+def action(datum):
+    prediction = compute_prediction(datum)
+    yield prediction
+
+def compute_prediction(datum):
+    x_score = coefs['x']*datum['x'] 
+    y_score = coefs['y']*datum['y'] 
+    prediction = x_score + y_score + coefs['intercept']
+    return prediction
+
+#modelop.metrics
+def metrics(data):
+    actuals = data.z.tolist()
+    data = data.to_dict(orient='records')
+    predictions = list(map(compute_prediction, data))
+    diffs = [x[0] - x[1] for x in zip(actuals, predictions)]
+    rmse = math.sqrt(sum(list(map(lambda x: x**2, diffs)))/len(diffs))
+    mae = sum(list(map(abs, diffs)))/len(diffs)
+    yield dict(MAE=mae, RMSE=rmse)
